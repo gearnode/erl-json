c2905a1cf347e73257717276f5134a5126fb07ab
Nicolas Martyanoff <khaelin@gmail.com>
2021-01-22 10:59:12 +0100

    inline most skip operations

--- src/json_parser.erl
+++ src/json_parser.erl
@@ -94,50 +94,50 @@ parse1(P0) ->
   {Value, skip_whitespace(P2)}.
 
 -spec parse_null(parser()) -> {null, parser()}.
-parse_null(P = #{data := <<"null", _/binary>>}) ->
-  {null, skip(P, 4)};
+parse_null(P = #{data := <<"null", Data/binary>>, column := Column}) ->
+  {null, P#{data => Data, column => Column+4}};
 parse_null(P) ->
   throw({error, parser_error(P, invalid_element)}).
 
 -spec parse_true(parser()) -> {true, parser()}.
-parse_true(P = #{data := <<"true", _/binary>>}) ->
-  {true, skip(P, 4)};
+parse_true(P = #{data := <<"true", Data/binary>>, column := Column}) ->
+  {true, P#{data => Data, column => Column+4}};
 parse_true(P) ->
   throw({error, parser_error(P, invalid_element)}).
 
 -spec parse_false(parser()) -> {false, parser()}.
-parse_false(P = #{data := <<"false", _/binary>>}) ->
-  {false, skip(P, 5)};
+parse_false(P = #{data := <<"false", Data/binary>>, column := Column}) ->
+  {false, P#{data => Data, column => Column+5}};
 parse_false(P) ->
   throw({error, parser_error(P, invalid_element)}).
 
 -spec parse_string(parser()) -> {binary(), parser()}.
-parse_string(P = #{data := <<$", _/binary>>}) ->
-  parse_string(skip1(P), <<>>);
+parse_string(P = #{data := <<$", Data/binary>>, column := Column}) ->
+  parse_string(P#{data => Data, column => Column+1}, <<>>);
 parse_string(P) ->
   throw({error, parser_error(P, invalid_string)}).
 
 -spec parse_string(parser(), Acc :: binary()) -> {binary(), parser()}.
 parse_string(P = #{data := <<>>}, _Acc) ->
   throw({error, parser_error(P, truncated_string)});
-parse_string(P = #{data := <<$", _/binary>>}, Acc) ->
-  {Acc, skip1(P)};
-parse_string(P = #{data := <<$\\, $", _/binary>>}, Acc) ->
-  parse_string(skip(P, 2), <<Acc/binary, $">>);
-parse_string(P = #{data := <<$\\, $\\, _/binary>>}, Acc) ->
-  parse_string(skip(P, 2), <<Acc/binary, $\\>>);
-parse_string(P = #{data := <<$\\, $/, _/binary>>}, Acc) ->
-  parse_string(skip(P, 2), <<Acc/binary, $/>>);
-parse_string(P = #{data := <<$\\, $b, _/binary>>}, Acc) ->
-  parse_string(skip(P, 2), <<Acc/binary, $\b>>);
-parse_string(P = #{data := <<$\\, $f, _/binary>>}, Acc) ->
-  parse_string(skip(P, 2), <<Acc/binary, $\f>>);
-parse_string(P = #{data := <<$\\, $r, _/binary>>}, Acc) ->
-  parse_string(skip(P, 2), <<Acc/binary, $\r>>);
-parse_string(P = #{data := <<$\\, $n, _/binary>>}, Acc) ->
-  parse_string(skip(P, 2), <<Acc/binary, $\n>>);
-parse_string(P = #{data := <<$\\, $t, _/binary>>}, Acc) ->
-  parse_string(skip(P, 2), <<Acc/binary, $\t>>);
+parse_string(P = #{data := <<$", Data/binary>>, column := Column}, Acc) ->
+  {Acc, P#{data => Data, column => Column+1}};
+parse_string(P = #{data := <<$\\, $", Data/binary>>, column := Column}, Acc) ->
+  parse_string(P#{data => Data, column => Column+2}, <<Acc/binary, $">>);
+parse_string(P = #{data := <<$\\, $\\, Data/binary>>, column := Column}, Acc) ->
+  parse_string(P#{data => Data, column => Column+2}, <<Acc/binary, $\\>>);
+parse_string(P = #{data := <<$\\, $/, Data/binary>>, column := Column}, Acc) ->
+  parse_string(P#{data => Data, column => Column+2}, <<Acc/binary, $/>>);
+parse_string(P = #{data := <<$\\, $b, Data/binary>>, column := Column}, Acc) ->
+  parse_string(P#{data => Data, column => Column+2}, <<Acc/binary, $\b>>);
+parse_string(P = #{data := <<$\\, $f, Data/binary>>, column := Column}, Acc) ->
+  parse_string(P#{data => Data, column => Column+2}, <<Acc/binary, $\f>>);
+parse_string(P = #{data := <<$\\, $r, Data/binary>>, column := Column}, Acc) ->
+  parse_string(P#{data => Data, column => Column+2}, <<Acc/binary, $\r>>);
+parse_string(P = #{data := <<$\\, $n, Data/binary>>, column := Column}, Acc) ->
+  parse_string(P#{data => Data, column => Column+2}, <<Acc/binary, $\n>>);
+parse_string(P = #{data := <<$\\, $t, Data/binary>>, column := Column}, Acc) ->
+  parse_string(P#{data => Data, column => Column+2}, <<Acc/binary, $\t>>);
 parse_string(P = #{data := <<$\\, C, _/binary>>}, Acc) when
     C =:= $u; C =:= $U ->
   {Code1, P2} = parse_unicode_escape_sequence(P),
@@ -153,17 +153,19 @@ parse_string(P = #{data := <<$\\, _, _/binary>>}, _Acc) ->
   throw({error, parser_error(P, invalid_escape_sequence)});
 parse_string(P = #{data := <<$\\>>}, _Acc) ->
   throw({error, parser_error(P, truncated_escape_sequence)});
-parse_string(P = #{data := (Data = <<C/utf8, Rest/binary>>)}, Acc) ->
+parse_string(P = #{data := (Data = <<C/utf8, Rest/binary>>),
+                   column := Column}, Acc) ->
   N = byte_size(Data) - byte_size(Rest),
-  parse_string(skip(P, N), <<Acc/binary, C/utf8>>).
+  parse_string(P#{data => Rest, column => Column+N}, <<Acc/binary, C/utf8>>).
 
 -spec parse_unicode_escape_sequence(parser()) -> {integer(), parser()}.
 parse_unicode_escape_sequence(P = #{data := <<$\\, C, HexCode:4/binary,
-                                              _/binary>>}) when
+                                              Data/binary>>,
+                                    column := Column}) when
     C =:= $u; C =:= $U ->
   try
     Code = binary_to_integer(HexCode, 16),
-    {Code, skip(P, 6)}
+    {Code, P#{data => Data, column => Column+6}}
   catch error:badarg ->
       throw({error, parser_error(P, invalid_escape_sequence)})
   end;
@@ -176,8 +178,10 @@ parse_unicode_escape_sequence(P) ->
   throw({error, parser_error(P, truncated_utf16_surrogate_pair)}).
 
 -spec parse_array(parser()) -> {[json:value()], parser()}.
-parse_array(P = #{data := <<$[, _/binary>>, depth := Depth}) ->
-  parse_array(skip_whitespace(skip1(P#{depth => Depth+1})), []);
+parse_array(P = #{data := <<$[, Data/binary>>, column := Column,
+                  depth := Depth}) ->
+  P2 = P#{data => Data, column => Column+1, depth => Depth+1},
+  parse_array(skip_whitespace(P2), []);
 parse_array(P) ->
   throw({error, parser_error(P, invalid_array)}).
 
@@ -188,14 +192,15 @@ parse_array(P = #{data := <<>>}, _Acc) ->
 parse_array(P = #{data := <<$], _/binary>>}, Acc) when
     Acc =/= [] ->
   throw({error, parser_error(P, {unexpected_character, $]})});
-parse_array(P = #{data := <<$], _/binary>>}, Acc) ->
-  {lists:reverse(Acc), skip1(P)};
+parse_array(P = #{data := <<$], Data/binary>>, column := Column}, Acc) ->
+  {lists:reverse(Acc), P#{data => Data, column => Column+1}};
 parse_array(P, Acc) ->
   case parse1(P) of
-    {Value, P2 = #{data := <<$,, _/binary>>}} ->
-      parse_array(skip_whitespace(skip1(P2)), [Value | Acc]);
-    {Value, P2 = #{data := <<$], _/binary>>}} ->
-      {lists:reverse([Value | Acc]), skip1(P2)};
+    {Value, P2 = #{data := <<$,, Data/binary>>, column := Column}} ->
+      parse_array(skip_whitespace(P2#{data => Data, column => Column+1}),
+                  [Value | Acc]);
+    {Value, P2 = #{data := <<$], Data/binary>>, column := Column}} ->
+      {lists:reverse([Value | Acc]), P2#{data => Data, column => Column+1}};
     {_, P2 = #{data := <<C, _/binary>>}} ->
       throw({error, parser_error(P2, {unexpected_character, C})});
     {_, P2 = #{data := <<>>}} ->
@@ -203,8 +208,10 @@ parse_array(P, Acc) ->
   end.
 
 -spec parse_object(parser()) -> {#{binary() := json:value()}, parser()}.
-parse_object(P = #{data := <<${, _/binary>>, depth := Depth}) ->
-  parse_object(skip_whitespace(skip1(P#{depth => Depth+1})), #{});
+parse_object(P = #{data := <<${, Data/binary>>, column := Column,
+                   depth := Depth}) ->
+  P2 = P#{data => Data, column => Column+1, depth => Depth+1},
+  parse_object(skip_whitespace(P2), #{});
 parse_object(P) ->
   throw({error, parser_error(P, invalid_object)}).
 
@@ -215,8 +222,8 @@ parse_object(P = #{data := <<>>}, _Acc) ->
 parse_object(P = #{data := <<$}, _/binary>>}, Acc) when
     map_size(Acc) > 0 ->
   throw({error, parser_error(P, {unexpected_character, $}})});
-parse_object(P = #{data := <<$}, _/binary>>}, Acc) ->
-  {Acc, skip1(P)};
+parse_object(P = #{data := <<$}, Data/binary>>, column := Column}, Acc) ->
+  {Acc, P#{data => Data, column => Column+1}};
 parse_object(P = #{options := Options}, Acc) ->
   case parse_object_member(P) of
     {{Key, Value}, P2} when is_binary(Key) ->
@@ -232,10 +239,11 @@ parse_object(P = #{options := Options}, Acc) ->
                  Acc#{Key => Value}
              end,
       case skip_whitespace(P2) of
-        P3 = #{data := <<$,, _/binary>>} ->
-          parse_object(skip_whitespace(skip1(P3)), Acc2);
-        P3 = #{data := <<$}, _/binary>>} ->
-          {Acc2, skip1(P3)};
+        P3 = #{data := <<$,, Data/binary>>, column := Column} ->
+          parse_object(skip_whitespace(P3#{data => Data, column => Column+1}),
+                       Acc2);
+        P3 = #{data := <<$}, Data/binary>>, column := Column} ->
+          {Acc2, P3#{data => Data, column => Column+1}};
         P3 = #{data := <<C, _/binary>>} ->
           throw({error, parser_error(P3, {unexpected_character, C})});
         P3 = #{data := <<>>} ->
@@ -248,8 +256,8 @@ parse_object(P = #{options := Options}, Acc) ->
 -spec parse_object_member(parser()) -> {{json:value(), json:value()}, parser()}.
 parse_object_member(P) ->
   case parse1(P) of
-    {Key, P2 = #{data := <<$:, _/binary>>}} ->
-      case skip_whitespace(skip1(P2)) of
+    {Key, P2 = #{data := <<$:, Data/binary>>, column := Column}} ->
+      case skip_whitespace(P2#{data => Data, column => Column+1}) of
         P3 = #{data := <<$}, _/binary>>} ->
           throw({error, parser_error(P3, {unexpected_character, $}})});
         P3 = #{data := <<>>} ->
@@ -280,54 +288,65 @@ parse_number(P) ->
                     Exponent :: undefined | integer()}) ->
         {number(), parser()}.
 %% Sign
-parse_number(P = #{data := <<$-, _/binary>>}, sign, {_, B, ES, E}) ->
-  parse_number(skip1(P), integer_part, {-1, B, ES, E});
+parse_number(P = #{data := <<$-, Data/binary>>, column := Column},
+             sign, {_, B, ES, E}) ->
+  parse_number(P#{data => Data, column => Column+1},
+               integer_part, {-1, B, ES, E});
 parse_number(P, sign, Acc) ->
   parse_number(P, integer_part, Acc);
 %% Integer part
-parse_number(P = #{data := (Data = <<C, _/binary>>)}, integer_part,
-             {S, _, ES, E}) when
+parse_number(P = #{data := (Data = <<C, _/binary>>), column := Column},
+             integer_part, {S, _, ES, E}) when
     C >= $0, C =< $9 ->
-  {I, Length} = parse_simple_integer(Data),
-  parse_number(skip(P, Length), fractional_part, {S, {integer, I}, ES, E});
+  {I, Rest, Length} = parse_simple_integer(Data),
+  parse_number(P#{data => Rest, column => Column+Length},
+               fractional_part, {S, {integer, I}, ES, E});
 parse_number(P, integer_part, _Acc) ->
       throw({error, parser_error(P, invalid_number)});
 %% Fractional part
-parse_number(P = #{data := <<$., Data/binary>>},
+parse_number(P = #{data := <<$., Data/binary>>, column := Column},
              fractional_part, {S, B, ES, E}) ->
   case Data of
     <<C, _/binary>> when C >= $0, C =< $9 ->
-      {F0, Length} = parse_simple_integer(Data),
+      {F0, Rest, Length} = parse_simple_integer(Data),
       F = F0 / math:pow(10, Length),
       I = case B of
             undefined -> 0;
             {integer, I2} -> I2
           end,
-      parse_number(skip(P, Length+1), exponent, {S, {float, I + F}, ES, E});
+      parse_number(P#{data => Rest, column => Column+Length+1},
+                   exponent, {S, {float, I + F}, ES, E});
     _ ->
       throw({error, parser_error(P, invalid_number)})
   end;
 parse_number(P, fractional_part, Acc) ->
   parse_number(P, exponent, Acc);
 %% Exponent
-parse_number(P = #{data := <<C, _/binary>>}, exponent, Acc) when
+parse_number(P = #{data := <<C, Data/binary>>, column := Column},
+             exponent, Acc) when
     C =:= $e; C =:= $E ->
-  parse_number(skip1(P), exponent_sign, Acc);
+  parse_number(P#{data => Data, column => Column+1}, exponent_sign, Acc);
 parse_number(P, exponent, Acc) ->
   parse_number(P, final, Acc);
 %% Exponent sign
-parse_number(P = #{data := <<$-, _/binary>>}, exponent_sign, {S, B, _, E}) ->
-  parse_number(skip1(P), exponent_part, {S, B, -1, E});
-parse_number(P = #{data := <<$+, _/binary>>}, exponent_sign, {S, B, _, E}) ->
-  parse_number(skip1(P), exponent_part, {S, B, 1, E});
+parse_number(P = #{data := <<$-, Data/binary>>, column := Column},
+             exponent_sign, {S, B, _, E}) ->
+  parse_number(P#{data => Data, column => Column+1},
+               exponent_part, {S, B, -1, E});
+parse_number(P = #{data := <<$+, Data/binary>>, column := Column},
+             exponent_sign, {S, B, _, E}) ->
+  parse_number(P#{data => Data, column => Column+1},
+               exponent_part, {S, B, 1, E});
 parse_number(P, exponent_sign, Acc) ->
   parse_number(P, exponent_part, Acc);
 %% Exponent part
-parse_number(P = #{data := (Data = <<C, _/binary>>)}, exponent_part,
+parse_number(P = #{data := (Data = <<C, _/binary>>), column := Column},
+             exponent_part,
              {S, B, ES, _}) when
     C >= $0, C =< $9 ->
-  {E, Length} = parse_simple_integer(Data),
-  parse_number(skip(P, Length), final, {S, B, ES, E});
+  {E, Rest, Length} = parse_simple_integer(Data),
+  parse_number(P#{data => Rest, column => Column+Length},
+               final, {S, B, ES, E});
 parse_number(P, exponent_part, _Acc) ->
       throw({error, parser_error(P, invalid_number)});
 %% Final state
@@ -341,21 +360,21 @@ parse_number(P, final, {S, {float, F}, ES, E}) ->
   {S * F * math:pow(10, E * ES), P}.
 
 -spec parse_simple_integer(binary()) ->
-        {I :: integer(), Length :: non_neg_integer()}.
+        {I :: integer(), Rest :: binary(), Length :: non_neg_integer()}.
 parse_simple_integer(Data) ->
   parse_simple_integer(Data, 0, 0).
 
 -spec parse_simple_integer(binary(), I :: integer(),
                            Length :: non_neg_integer()) ->
-        {I :: integer(), Length :: non_neg_integer()}.
+        {I :: integer(), Rest :: binary(), Length :: non_neg_integer()}.
 parse_simple_integer(<<>>, I, Length) ->
-  {I, Length};
-parse_simple_integer(<<C, Rest/binary>>, I, Length) ->
+  {I, <<>>, Length};
+parse_simple_integer(Data = <<C, Rest/binary>>, I, Length) ->
   case is_digit(C) of
     true ->
       parse_simple_integer(Rest, I*10 + C-$0, Length+1);
     false ->
-      {I, Length}
+      {I, Data, Length}
   end.
 
 -spec is_digit(integer()) -> boolean().
@@ -370,15 +389,3 @@ skip_whitespace(P = #{data := <<C, Data/binary>>, column := Column}) when
   skip_whitespace(P#{data => Data, column => Column+1});
 skip_whitespace(P) ->
   P.
-
--spec skip(parser(), non_neg_integer()) -> parser().
-skip(P, 0) ->
-  P;
-skip(P, N) ->
-  skip(skip1(P), N-1).
-
--spec skip1(parser()) -> parser().
-skip1(P = #{data := <<$\n, Data/binary>>, line := Line}) ->
-  P#{data => Data, line => Line+1, column => 0};
-skip1(P = #{data := <<_, Data/binary>>, column := Column}) ->
-  P#{data => Data, column => Column+1}.
